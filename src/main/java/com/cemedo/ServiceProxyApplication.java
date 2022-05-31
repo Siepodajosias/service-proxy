95f2bd7c46a7659cfa0aaa41a5499dc284aa3d82
Siepodajosias <88053473+Siepodajosias@users.noreply.github.com>
2022-05-31 16:24:57 +0000

    premier commit de l'application proxy

---
+++ src/main/java/com/cemedo/ServiceProxyApplication.java
@@ -0,0 +1,21 @@
+package com.cemedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ServiceProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProxyApplication.class, args);
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynaRout(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}
+}
---
+++ src/test/java/com/cemedo/ServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cemedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
